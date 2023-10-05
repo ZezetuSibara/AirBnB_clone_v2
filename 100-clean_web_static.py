75358e080741ccf3047e9ce6dae45a7d39409552
ZezetuSibara <zezethu0730@gmail.com>
2023-10-05 10:26:53 +0000

    0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,43 +1,70 @@
 #!/usr/bin/python3
-"""Clean all archives based on the number of
-arguements passed"""
+""" Program that keep cleant the version folder of web static """
+from datetime import datetime
+from fabric.api import *
+from os import path
 
-from operator import length_hint
-from fabric.api import run, local, cd, env
-import os
+env.hosts = ['34.75.163.65', '3.229.120.59']
 
-# env.hosts = ['3.235.198.120', '3.239.50.204']
+
+def do_pack():
+    """ Generates a .tgz archive from the contents of the web_static
+    folder of your AirBnB Clone repo """
+    date_str = datetime.now().strftime('%Y%m%d%H%M%S')
+    local("mkdir -p versions/")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(date_str))
+        return "versions/web_static_{}.tgz".format(date_str)
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """ Distributes an archive to the web servers """
+    if not path.exists(archive_path):
+        return False
+    # split the path and get the second element in the list
+    file_path = archive_path.split("/")[1]
+    serv_folder = "/data/web_static/releases/" + file_path
+
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p " + serv_folder)
+        run("sudo tar -xzf /tmp/" + file_path + " -C " + serv_folder + "/")
+        run("sudo rm /tmp/" + file_path)
+        run("sudo mv " + serv_folder + "/web_static/* " + serv_folder)
+        run("sudo rm -rf " + serv_folder + "/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s " + serv_folder + " /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """ Call the do_pack() function and store the path of the created archive
+    Call the do_deploy(archive_path) function, using the new
+    path of the new archive
+    Return False if no archive has been created
+    Return the return value of do_deploy"""
+    file_path = do_pack()
+    if file_path is None:
+        return False
+
+    return (do_deploy(file_path))
 
 
 def do_clean(number=0):
-    """Cleans all .tgz files"""
-    """if os.path.exists('versions'):
-        # with cd('versions'):
-        # local('find ')
-        path = 'versions'
-        files = [file for file in os.listdir(
-            path) if 'web' in file and '.tgz' in file]
-
-        print(files)
-
-        length = len(files)
-        if int(number) > length:
-            exit
-        if int(number) == 0 or int(number) == 1:
-            last = 1
-        else:
-            last = int(number)
-
-        if files:
-            for index in range(length - last):
-              local('rm versions/{}'.format(files[index]))"""
-
+    """ Deletes out-of-date archives
+    number is the number of the archives, including the most recent, to keep"""
     number = int(number)
-    if number == 0:
-        number = 2
+    # keep only the most recent version of your archive
+    if (number == 1 or number == 0):
+        local('cd versions; ls -t | head -n -1 | xargs rm -rf')
+        run('cd /data/web_static/releases; ls -t | head -n -1 | xargs rm -rf')
     else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+        local('cd versions; ls -t | head -n -{} | xargs rm -rf'.format(number))
+        run('cd /data/web_static/releases; ls -t | head -n -{} | xargs rm -rf'.
+            format(number))
